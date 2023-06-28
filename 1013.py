152f5ec00dee3dd685b9924d4c2314ba2d77ee22
beaferreira <triszferreira2@gmail.com>
2023-06-28 18:30:28 -0300

    1013 finalizado

--- 1013.py
+++ 1013.py
@@ -6,3 +6,8 @@ c = int(c)
 
 maiorAB = (a+b+abs(a-b)) / 2 
 
+maior = (maiorAB+c+abs(maiorAB-c)) / 2
+
+maior = int(maior)
+
+print(maior, "eh o maior")
---
+++ 1014.py
@@ -0,0 +1,8 @@
+x = int(input())
+y = float(input())
+
+calculo = x / y
+
+calculo = round(calculo, 3)
+
+print((calculo), "km/l")

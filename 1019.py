a7caff475770f6fecf0af3ed82f1da3daf0988e6
gabrielfontineli <gabriel0fontineli@gmail.com>
2023-07-12 18:44:00 -0300

    1019

--- 1019.py
+++ 1019.py
@@ -1,3 +1,8 @@
-gostosa = 'beatriz'
-if 'bea' in gostosa:
-    print(True)
+N = int(input())
+
+horas = N // 3600
+minutos = (N % 3600) // 60
+segundos = N % 60 
+
+tempo_formatado = "{:01d}:{:01d}:{:01d}".format(horas, minutos, segundos)
+print(tempo_formatado)
